146640eea262b0e807748dd93759452cb1a23cb7
Mst <bilkis.naher@yahoo.co.uk>
2025-01-12 17:59:46 +0000

    add Node's argument value files

---
+++ 01-Process-Argvalue/index.js
@@ -0,0 +1,6 @@
+// console.log(process.argv);
+
+
+console.log(process.argv[2]);
+
+
---
+++ 02-Parameter-Check/index.js
@@ -0,0 +1,19 @@
+const a = process.argv[2];
+const b = process.argv[3];
+ 
+// using a conditional statement:
+ if( a === b){
+console.log(true);
+ }else{
+    console.log(false);
+ }
+
+//  Usinig a ternary operator:
+
+console.log( a === b ? true : false)
+
+// Compairing Variables
+console.log( a === b);
+
+// Comparing argument values directly
+console.log(process.argv[2] === process.argv[3]);
