28ea28a06be890498089a22d135cfbc7696f5610
MST <bilkis.naher@yahoo.co.uk>
2025-01-15 12:23:37 +0000

    update the badmath file

--- 06-modularization/badmath.js
+++ 06-modularization/badmath.js
@@ -2,6 +2,14 @@ const pie = 'apple';
 
 const predictable = () => 1;
 
+// module.exports is an object, we use to store variables or methods.
+//In this export object we are using property value shorthand
+
+// Property value shorthand:
+// #If we want to define a property in an object, 
+// and that key name already exists as a variable with in the 
+// object's scope, we don't have to explicitly assign a value. 
+
 module.exports = {
     pie,
     predictable,
